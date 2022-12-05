eb02ece7e8c0690104606f0b79f9a9c593023593
zhukv98 <76707463+zhukv98@users.noreply.github.com>
2022-12-04 19:46:44 -0500

    Additional updates

--- source.py
+++ source.py
@@ -23,14 +23,14 @@
 # *How will you use this data to answer your project question?*
 # 
 # Data Sources:
-# - https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/ (Note: This needs an API key. Go to https://rapidapi.com/axisbits-axisbits-default/api/covid-19-statistics/)
+# - https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/ (Note: This needs an API key. Go to https://rapidapi.com/vaccovidlive-vaccovidlive-default/api/vaccovid-coronavirus-vaccine-and-treatment-tracker/)
 # - EO_14102022180121049.csv (URL: https://stats.oecd.org/Index.aspx?DatasetCode=STLABOUR#)
 # - EO_14102022183231815.csv (URL: https://stats.oecd.org/Index.aspx?DatasetCode=STLABOUR#)
 # - https://covid19.who.int/who-data/vaccination-data.csv
 # 
 # The 2 datasets will show the current COVID cases that includes their severity, deaths, testing, and vaccination levels by country. GDP growth and Unemployment data of each country will help explain the economic and financial consequences of COVID-19. The more people adapt to the COVID-19 Pandemic, the easier it is to get used to it and keep on moving. 
 
-# In[32]:
+# In[1]:
 
 
 # Start your code here
@@ -95,7 +95,7 @@ headers = {
 response = requests.request("GET", url, headers=headers)
 
 
-# In[33]:
+# In[2]:
 
 
 # Database file
@@ -103,27 +103,27 @@ gdp_growth_df = pd.read_csv('EO_14102022183231815.csv')
 pd.set_option('display.max_columns', None)
 
 
-# In[34]:
+# In[3]:
 
 
 unemployment_df = pd.read_csv('EO_14102022180121049.csv')
 
 
-# In[35]:
+# In[4]:
 
 
 # Database or File URL
 vaccination_df = pd.read_csv('https://covid19.who.int/who-data/vaccination-data.csv')
 
 
-# In[36]:
+# In[5]:
 
 
 json = response.json()
 covid_df = pd.DataFrame(json)
 
 
-# In[37]:
+# In[6]:
 
 
 display(covid_df.describe())
@@ -134,7 +134,7 @@ display(covid_df.isnull().sum())
 
 # Showing descriptive statistics, correlation, and information about the dataset that holds the current cases, deaths, and testing data on COVID-19 in each country. It also includes the first 2 rows that both show total amount of cases, deaths, and testing globally. Only TwoLetterSymbol and TwoLetterSymbol have missing data that is also found on those same rows after analyzing it. Those are not needed so it should be removed. Besides that, no duplicated date found in this dataset. I noticed that some test percentage values are above 100%. From what I could tell, people tend to get tested more than once for specific reasons that include going to events or travel to places.
 
-# In[39]:
+# In[7]:
 
 
 covid_df.dropna(inplace=True)
@@ -143,7 +143,7 @@ display(covid_df.isnull().sum())
 
 # Removed rows that have missing data.
 
-# In[40]:
+# In[8]:
 
 
 display(px.bar(covid_df.sample(20), x='Country', y='Infection_Risk', labels={'Infection_Risk':'Infection Risk (%)'}, title='Infection Risk by Country'))
@@ -151,7 +151,7 @@ display(px.bar(covid_df.sample(20), x='Country', y='Infection_Risk', labels={'In
 
 # Presenting a bar chart using plotly.express to show infection risk levels from 20 random countries. These risk levels will show the chances of getting infected at these countries. Some governments take extreme measures like enforcing mandatory quarantine to contain the virus while others are struggling with their main industry like tourism. That in turn can cause decline in economic performance.
 
-# In[41]:
+# In[9]:
 
 
 display(gdp_growth_df.describe())
@@ -161,7 +161,7 @@ display(gdp_growth_df.info())
 
 # Showing descriptive statistics, correlation, and information about the dataset that holds quarterly gdp growth from each country, starting from first quarter of 2019 to the projected GDP growth in 2023. No correlations are found in this dataset due to seeing missing data on all integer columns except the value column itself. I can change either "Time" or "TIME" column to date/time data type if I'm going to use them for a graph or other visualization. "Flag Codes" and "Flags" columns are empty so they will be removed.
 
-# In[42]:
+# In[10]:
 
 
 if('Flag Codes', 'Flags') in gdp_growth_df.columns:
@@ -171,7 +171,7 @@ gdp_growth_df['TIME'] = pd.to_datetime(gdp_growth_df['TIME'])
 
 # Removing empty "Flag Codes" and "Flags" columns and changed "TIME" column to datetime data type. The "TIME" column will be used to show the time progression of GDP growth in each country and understand its impact from COVID-19 pandemic.
 
-# In[43]:
+# In[11]:
 
 
 display(px.line(gdp_growth_df, x='TIME', y='Value', color='Country', labels={'Value':'GDP Growth (%)'}, title='GDP Growth Progression'))
@@ -179,7 +179,7 @@ display(px.line(gdp_growth_df, x='TIME', y='Value', color='Country', labels={'Va
 
 # This is a line graph from plotly express. This shows GDP growth of each country over time. As you can see, some countries didn't adapt as quickly as others at the height of the pandemic in 2020 which explains the moderate to sharp drop in GDP growth. 
 
-# In[44]:
+# In[12]:
 
 
 display(unemployment_df.describe())
@@ -190,7 +190,7 @@ display(unemployment_df.info())
 
 # Showing descriptive statistics, correlation, information, and boxplot on the dataset that holds quarterly unemployment rate from each country, starting from first quarter of 2019 to the projected GDP growth in 2023. No correlations are found in this dataset due to seeing missing data on all integer columns except the value column itself. I can change either "Time" or "TIME" column to date/time data type if I'm going to use them for a graph or other visualization. "Flag Codes", "Flags", "Reference Period Code",and "Reference Period" columns are empty so they will be removed.
 
-# In[45]:
+# In[13]:
 
 
 unemployment_box = sns.boxplot(data=unemployment_df, x='TIME', y='Value')
@@ -199,7 +199,7 @@ unemployment_box.set_xticklabels(labels=unemployment_box.get_xticklabels(), rota
 
 # Used boxplot from seaborn visualization library. Rotated TIME labels by 90 degrees prevent them from being overlapped by each other. From the boxplot above, there are some outliers in the unemployment rate on each of the "TIME" columns.
 
-# In[46]:
+# In[14]:
 
 
 if('Flag Codes', 'Flags', 'Reference Period Code', 'Reference Period') in unemployment_df.columns:
@@ -209,7 +209,7 @@ unemployment_df['TIME'] = pd.to_datetime(unemployment_df['TIME'])
 
 # Removed "Flag Codes", "Flags", "Reference Period Code", "Reference Period" columns because they are not relevant to this project. Changing "TIME" column to datetime data type.
 
-# In[47]:
+# In[15]:
 
 
 display(px.line(unemployment_df, x='TIME', y='Value', color='Country', labels={'Value':'Unemployment Rate (%)'}, title='Unemployment by Country'))
@@ -217,7 +217,7 @@ display(px.line(unemployment_df, x='TIME', y='Value', color='Country', labels={'
 
 # Here's the line graph from plotly express. This shows the unemployment rates at each country over time during the COVID-19 pandemic. It gives the insight on how well each country has adapted during the COVID-19 Pandemic.
 
-# In[48]:
+# In[16]:
 
 
 display(vaccination_df.describe())
@@ -228,7 +228,7 @@ display(vaccination_df.info())
 
 # Almost all the columns from the dataset above have positive correlations with each other. I noticed that some columns have missing data including first vaccine date and amount of people who got the booster. The median and the 50th Percentile values on each column are the same in the vaccination dataset.
 
-# In[51]:
+# In[17]:
 
 
 vaccination_df.dropna(inplace=True)
@@ -240,7 +240,7 @@ vaccination_df['WHO_REGION'] = vaccination_df['WHO_REGION'].replace(['OTHER'], '
 
 # Removing rows that have missing data from the vaccination dataset. Since Liechtenstein is has the value of 'OTHER' in the WHO_REGION column, it is treated as 'EURO' since it's part of Europe.
 
-# In[52]:
+# In[18]:
 
 
 sns.barplot(data=vaccination_df.sample(10), x='COUNTRY', y='PERSONS_FULLY_VACCINATED_PER100').set(
@@ -252,7 +252,7 @@ plt.show()
 
 # This is a bar graph from seaborn library. It shows the number of people fully vaccinated per 100 by each country based on most recent data. This gives an insight on how far each country has progressed to get their citizens full vaccinated. Businesses evolve over time to get their employees back to work while at the same time, minimize infections so that they can smoothly and efficiently recover their losses from the pandemic.
 
-# In[53]:
+# In[19]:
 
 
 sns.scatterplot(data=vaccination_df, x='PERSONS_VACCINATED_1PLUS_DOSE_PER100',
@@ -272,7 +272,7 @@ sns.set(rc={"figure.figsize": (10, 10)})
 
 # Several cells done shows working with vaccine training dataset. Calculating the AUC scores using the same classifiers from our labs.
 
-# In[54]:
+# In[20]:
 
 
 vaccine_train_set, vaccine_test_set = train_test_split(vaccination_df, test_size=0.2, random_state=32)
@@ -282,7 +282,7 @@ display(vaccine_test_set.head())
 
 # Separate training and test sets for vaccination data.
 
-# In[63]:
+# In[21]:
 
 
 split = StratifiedShuffleSplit(test_size=0.2, random_state=42)
@@ -293,14 +293,14 @@ for train_index, test_index in split.split(vaccination_df, vaccination_df['WHO_R
 
 # Selecting a startified shuffle split for the training and testing data. Similar to labs.
 
-# In[64]:
+# In[22]:
 
 
 vaccine_X = region_strat_train_set.drop('WHO_REGION', axis=1)
 vaccine_y = region_strat_train_set[['WHO_REGION']].copy()
 
 
-# In[65]:
+# In[23]:
 
 
 num_features = ['TOTAL_VACCINATIONS',	'PERSONS_VACCINATED_1PLUS_DOSE', 'TOTAL_VACCINATIONS_PER100',	'PERSONS_VACCINATED_1PLUS_DOSE_PER100',	'PERSONS_FULLY_VACCINATED',	'PERSONS_FULLY_VACCINATED_PER100',	'NUMBER_VACCINES_TYPES_USED',	'PERSONS_BOOSTER_ADD_DOSE',	'PERSONS_BOOSTER_ADD_DOSE_PER100']
@@ -333,7 +333,7 @@ vaccine_prepared = pd.DataFrame(vaccine_prepared.toarray(), columns=column_names
 vaccine_prepared.head()
 
 
-# In[69]:
+# In[24]:
 
 
 for model in [
@@ -359,7 +359,7 @@ for model in [
 
 # Selecting a model with the best performance which is Random Forset Classifier unless otherwise.
 
-# In[70]:
+# In[25]:
 
 
 param_grid = {
@@ -378,7 +378,7 @@ print(f"Best Params: {grid_search.best_params_}")
 
 # Making improvements for the Random Forest Classifier model.
 
-# In[72]:
+# In[26]:
 
 
 vaccine_test_X = region_strat_test_set.drop('WHO_REGION', axis=1)
@@ -390,7 +390,7 @@ updated_gradient_boosting_classifier.fit(vaccine_test_set, vaccine_test_y)
 print("AUC Score: {}".format(updated_gradient_boosting_classifier.score(vaccine_test_set, vaccine_test_y)))
 
 
-# In[74]:
+# In[27]:
 
 
 get_ipython().system('jupyter nbconvert --to python source.ipynb')
